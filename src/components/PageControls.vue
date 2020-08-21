e50048b005d74ce3a939ff73cb49c705fb1fb9f4
Paul Ezaga <paulezaga@gmail.com>
2020-08-21 13:32:35 +0100

    Pagination Modified for Large DataSets

--- src/components/PageControls.vue
+++ src/components/PageControls.vue
@@ -1,22 +1,37 @@
 <template>
     <div class="row mt-2">
-        <div class="col form-group">
-            <select class="form-control" v-model="pageSize" v-on:change="setPageSize(pageSize)">
+        <div class="col-3 form-group">
+            <select class="form-control" v-model="pageSize"  v-on:change="setPageSize(pageSize)">
                 <option value="4">4 per Page</option>
                 <option value="8">8 per Page</option>
                 <option value="12">12 per Page</option>
             </select>
         </div>
-        <div class="text-right col">        
-            <div class="btn-gorup mx-2">
-                <button v-for="page in pageNumbers()" :key="page" class="btn"
-                    v-bind:class="{ 'btn-primary' : page == currentPage, 'btn-secondary' : page != currentPage }"
-                    v-on:click="setCurrentPage(page)">
-                    {{ page }}
-                </button>
-            </div>        
-        </div>  
-    </div>  
+        <div class="text-right col">
+            <button v-bind:disabled="currentPage == 1"
+            v-on:click="setCurrentPage(currentPage - 1)"
+            class="btn btn-secondary mx -1">Previous</button>
+            <span v-if="currentPage > 4">
+                <button v-on:click="setCurrentPage(1)"
+                class="btn btn-secondary mx-1">1</button>
+                <span class="h4">...</span>
+            </span>
+            <span class="mx-1">
+                <button v-for="i in pageNumbers" v-bind:key="i"
+                class="btn btn-secpmdary"
+                v-bind:class="{ 'btn-primary': i == currentPage }"
+                v-on:click="setCurrentPage(i)">{{ i }}</button>
+            </span>
+            <span v-if="currentPage <= pageCount - 4">
+                <span class="h4">...</span>
+                <button v-on:click="setCurrentPage(pageCount)"
+                class="btn btn-secondary mx-1">{{ pageCount}}</button>
+            </span>
+            <button v-bind:disabled="currentPage == pageCount"
+            v-on:click="setCurrentPage(currentPage + 1)"
+            class="btn btn-secondary mx-1">Next</button>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -32,10 +47,17 @@ export default class PageControls extends Vue {
     private pageSize = 4;
 
     private pageNumbers(){
-        const arrLength = this.pageCount + 1;
-        const res = [...new Array(arrLength).keys()].slice(1);
-        return res;
+        if(this.pageCount < 4){
+            return [...Array(this.pageCount + 1).keys()].slice(1);
+        } else if(this.currentPage <= 4 ){
+            return [1, 2, 3, 4, 5];
+        } else if(this.currentPage > this.pageCount - 4){
+            return [...Array(5).keys()].reverse()
+                .map(v => this.pageCount - v)
+        }else {
+            return [this.currentPage -1, this.currentPage, this.currentPage + 1];
+        }
     }
-    
+
 }
 </script>
